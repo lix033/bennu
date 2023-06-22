77f804da9c8dbc974e570adc9997c1b5ed5d5cfa
lix033 <julienkolou03@gmail.com>
2023-06-22 09:42:39 +0000

    shipment 2

---
+++ main2.js
@@ -0,0 +1,35 @@
+fetch('shipment2.json')
+  .then(response => response.json())
+  .then(data => {
+    const sessionsList = document.querySelector('.sessions');
+
+    data.forEach(event => {
+      const eventDateTime = new Date(event.eventDateTime);
+      const eventTime = eventDateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+      const eventDescription = `${event.eventPosition.status}`;
+      const eventCity = `${event.eventPosition.city}, ${event.eventPosition.country}`;
+    //   const eventCountry = `${event.eventPosition.status}: ${event.eventPosition.comments}`;
+      
+      const listItem = document.createElement('li');
+
+      listItem.innerHTML = `
+        <div class="time">${eventTime}</div>
+        <p>${eventDescription}</p><span style="margin-left:40px;">${eventCity}</span>
+      `;
+
+      if (eventDescription === 'ARRIVED AT YOUR PLACE') {
+        listItem.classList.add('arrived');
+      } else if (eventDescription === 'DELIVERED') {
+        listItem.classList.add('delivered');
+      }
+    //   else if (eventDescription === 'PICK UP DELAYED'){
+    //     listItem.classList.add('expetion')
+    //   }
+
+      if (event.shipment.status.shipmentIsDelayed === 1) {
+        listItem.classList.add('delayed');
+      }
+
+      sessionsList.appendChild(listItem);
+    });
+  });
